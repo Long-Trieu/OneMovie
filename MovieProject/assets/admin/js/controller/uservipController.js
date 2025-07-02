7690ff31fea0c6fb23f9725acc11ba923de1f643
Trieu Long <trieungoclong0811@gmail.com>
2025-07-02 21:38:17 +0700

    Cap nhat

---
+++ MovieProject/assets/admin/js/controller/uservipController.js
@@ -0,0 +1,28 @@
+﻿var user = {
+    init: function () {
+        user.registerEvents();
+    },
+    registerEvents: function () {
+        $('.btn-default2').off('click').on('click', function (e) {
+            e.preventDefault();
+            var btn = $(this);
+            var id = $(btn).data('id');
+            $.ajax({
+                url: "/Admin/User/ChangeVip",
+                data: { id: id },
+                dataType: "json",
+                type: "POST",
+                success: function (response) {
+                    console.log(response);
+                    if (response.vip == true) {
+                        btn.text("Kích hoạt");
+                    }
+                    else {
+                        btn.text("Huỷ Kích hoạt");
+                    }
+                }
+            });
+        });
+    }
+}
+user.init();
